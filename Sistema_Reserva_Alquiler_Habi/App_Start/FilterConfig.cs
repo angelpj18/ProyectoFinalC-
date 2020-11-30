5f5eaa20a6f644d2ef0f6411d0aa991b93e23ac1
angelpj18 <angelab199824@gmail.com>
2020-11-30 18:56:10 -0300

    Agregar archivos de proyecto.

---
+++ Sistema_Reserva_Alquiler_Habi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_Reserva_Alquiler_Habi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Sistema_Reserva_Alquiler_Habi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sistema_Reserva_Alquiler_Habi.MvcApplication" Language="C#" %>
